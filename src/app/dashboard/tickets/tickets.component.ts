07f845062066b219d20baadcc102d2f48a7143be
ipavlakos <pavlakosIlias@outlook.com>
2024-08-16 14:04:08 +0300

    Component outputs

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NewTicketComponent} from "./new-ticket/new-ticket.component";
 import {Ticket} from "./ticket/ticket.model";
 
@@ -12,5 +12,15 @@ import {Ticket} from "./ticket/ticket.model";
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
-  tickets : Ticket[] = [];
+  tickets: Ticket[] = [];
+
+  onAdd(ticketData: { title: string, text: string }) {
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request: ticketData.text,
+      id: Math.random().toString(),
+      status: 'open'
+    };
+    this.tickets.push(ticket);
+  }
 }
