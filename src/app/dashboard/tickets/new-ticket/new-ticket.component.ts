887c2df2ca3ee12d1caa4300f61b933efb8b5761
ipavlakos <pavlakosIlias@outlook.com>
2024-08-16 13:02:36 +0300

    Using the viewChild signal function

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, viewChild} from '@angular/core';
 import {ButtonComponent} from "../../../shared/button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
 import {FormsModule} from "@angular/forms";
@@ -15,10 +15,10 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
-
+  //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
